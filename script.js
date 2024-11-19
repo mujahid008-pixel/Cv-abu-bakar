c434244a912eb02e585d0487cca34f6f0b276b82
mujahid008-pixel <mujahidabubakar235@gmail.com>
2024-11-19 10:59:54 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -64,3 +64,25 @@ sections.forEach((section) => {
     observer.observe(section);
 });
 
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+  
+  document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+  
